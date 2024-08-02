feb889cda80ff4db515fc0d16d6665c414f02416
Sven Meyer <sven.meyer@adapton.de>
2024-08-02 11:04:51 +0200

    Cleaned up

--- i3/scripts/lock.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-IMG=/tmp/screenshot.png
-flameshot full -r > $IMG
-convert $IMG -scale 25% -scale 400% $IMG
-i3lock -p win -i $IMG
--- install.sh
+++ install.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 
-TILDE="~"
+OLDPWD="$(pwd -P)"
 DOTFILES="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-DOTFILES="${DOTFILES/${HOME}/$TILDE}"
 
-if [[ -d $HOME/.config ]]; then
-  mkdir -p $HOME/.config/pipewire
-fi
+cd $DOTFILES
+git submodule init
+git submodule update
 
-for app in alacritty i3 picom polybar rofi
-do
-  ln -s $DOTFILES/$app $HOME/.config/.$app
-done
-
-ln -s $DOTFILES/pipewire/t450/media-session.d $HOME/.config/pipewire/media-session.d
-
+# nerd fonts
 ln -s $DOTFILES/fonts $HOME/.fonts
 
+# tmux
 for cfg in $(ls $DOTFILES/tmux)
 do
  ln -s $DOFILES/tmux/$file $HOME/.$file
 done
 
+# vim
 mkdir $HOME/.vim
 ln -s $DOTFILES/vim/vimrc $HOME/.vimrc
 ln -s $DOTFILES/vim/autoload $HOME/.vim/autoload
 ln -s $DOTFILES/vim/bundle $HOME/.vim/bundle
 
+# omz
 ln -s $DOTFILES/zsh/zshrc $HOME/.zshrc
 ln -s $DOTFILES/zsh/p10k.zsh $HOME/.p10k.zsh
 ln -s $DOTFILES/zsh/ohmyzsh $HOME/.ohmyzsh
 ln -s $DOTFILES/zsh/powerlevel10k $HOME/.ohmyzsh/custom/themes/powerlevel10k
+
+cd $OLDPWD
