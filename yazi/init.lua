5678be77fe846a18fb068427c1487293c90fdd65
Sven Meyer <sven.meyer@adapton.de>
2025-07-04 11:03:05 +0200

    Recent changes

---
+++ yazi/init.lua
@@ -0,0 +1,40 @@
+th.git = th.git or {}
+th.git.modified = ui.Style():fg("blue")
+th.git.deleted = ui.Style():fg("red"):bold()
+th.git.modified_sign = "M"
+th.git.deleted_sign = "D"
+th.git.ignored_sign = "!"
+th.git.added_sign = "A"
+
+
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
+
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if not h or ya.target_family() ~= "unix" then
+		return ""
+	end
+
+	return ui.Line {
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		":",
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		" ",
+	}
+end, 500, Status.RIGHT)
+
+
+require("full-border"):setup()
+require("git"):setup()
+require("githead"):setup()
+require("zoxide"):setup {
+  update_db = true
+}
+
